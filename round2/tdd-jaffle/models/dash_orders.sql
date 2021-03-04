9e8e68050159d2352ee284fc05e50b30ac225b84
Sven Balnojan <sven.balnojan@mercateo.com>
2021-03-04 10:16:51 +0100

    add round2

---
+++ round2/tdd-jaffle/models/dash_orders.sql
@@ -0,0 +1,3 @@
+-- Use the `ref` function to select from other models
+-- Produce some dummy data...
+select "first_order"
---
+++ round2/tdd-jaffle/tests/round2.sql
@@ -0,0 +1,12 @@
+-- checks for any order at all
+with result as (select
+    order_count, date
+from {{ ref('dash_orders' )}}),
+
+count as (select case when count(*) > 0 then 1
+          else 0
+          end res
+ from result)
+ select * from count group by 1
+
+having (res = 0)
